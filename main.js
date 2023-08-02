84c9d343a67b58f3b34a44b7829a340103a243bc
Marochigit <marochilucas3@gmail.com>
2023-08-01 21:10:28 -0300

    PreEntrega2 Marochi

--- main.js
+++ main.js
@@ -1,49 +1,104 @@
-alert("Calcule las faltas de los estudiantes")
-const usuarioHabilitado = "profesor"
-const contraseniaHabilitada = "contraseña"
+//Mini Programa que crea clientes de netflix 
 
-let usuario = prompt("Ingrese su usuario");
-let contrasenia = prompt("Ingrese su contraseña")
+// Cliente
 
-if(usuario === usuarioHabilitado && contrasenia === contraseniaHabilitada){
-    alert ("Bienvenido " + usuario)
-
-    let cantidadAlumnos = parseInt(prompt("Ingrese la cantidad de estudiantes que quiera saber sus faltas"))
-
-    for(let i = 0 ; i < cantidadAlumnos ; i++){
-
-        let nombreEstudiante = prompt("Ingrese el nombre del alumno");
-        let falta1 = parseInt(prompt("Ingrese las faltas del primer trimestre"));
-        let falta2 = parseInt(prompt("Ingrese las faltas del segundo trimestre"));
-        let falta3 = parseInt(prompt("Ingrese las faltas del tercer trimestre"));
-        let faltas = calcularFaltas(falta1, falta2, falta3);
-        alert("Las falatas de " + nombreEstudiante + " son " + faltas);
-
-        let total = totalFaltas(faltas)
-
-        if(total){
-            alert (nombreEstudiante + " es regular");
-        } else{
-            alert (nombreEstudiante + " quedo libre")
-        }
+class Cliente {
+    constructor(nombre, apellido, dni, cuenta){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.cuenta = cuenta;
     }
-} else{
-    alert("Usuario o contraseña incorrecta. Intente de nuevo")
 }
 
+const clienteAndrea = new Cliente("Andrea", "Portela", 25556614, "Premiun")
+const clienteNicolas = new Cliente("Nicolas", "Fulgenzi", 45692952, "Basic")
+const clienteLucas = new Cliente("Lucas", "Marochi", 45692918, "Basic")
 
-//Funciones 
+const arrayClientes = [clienteAndrea, clienteNicolas, clienteLucas]
+console.log(arrayClientes)
 
+// Funcion (Menú de opciones)
 
-function calcularFaltas(falta1, falta2, falta3){
-    let faltas = (falta1 + falta2 + falta3);
-    return faltas;
+function menu(){
+    alert("Bienvenido al creador auntomatico de clientes de Netflix!!! ");
+    let opcion = parseInt(prompt("Ingrese una opcion: 1)Alta de Cliente   2)Baja de Cliente   3)Modificacion de Cliente   4)Consulta de Cliente   5)Salir"));
+    return opcion;
 }
 
-function totalFaltas(faltas){
-    if(faltas <= 15){
-        return true;
-    } else {
-        return false;
-    }
+// Funcion para dar de alta a un cliente
+
+function altaCliente() {
+    let nombre = prompt("Ingrese el Nombre del cliente: ");
+    let apellido = prompt("Ingrese el Apellido del cliente: ");
+    let dni = prompt("Ingrese el DNI del cliente: ");
+    let cuenta = prompt("Ingrese el tipo de cuenta que desea: ");
+    let cliente = new Cliente (nombre, apellido, dni, cuenta);
+    arrayClientes.push(cliente);
+    console.log(arrayClientes);
+}
+
+// Funcion para dar de baja a un cliente
+
+function bajaCliente(){
+    let dni = parseInt(prompt("Ingrese el DNI del clinte: "));
+    let cliente = arrayClientes.find(cliente => cliente.dni === dni);
+    let indice = arrayClientes.indexOf(cliente);
+    arrayClientes.splice(indice,1);
+    console.log(arrayClientes);
+}
+
+// Funcion para modificar datos de un cliente
+
+function modificacionCliente(){
+    let dni = parseInt(prompt("Ingrese el DNI del clinte: "));
+    let cliente = arrayClientes.find(cliente => cliente.dni === dni);
+    let indice = arrayClientes.indexOf(cliente);
+
+    let nombre = prompt("Ingrese el nombre del cliente: ");
+    let apellido = prompt("Ingrese el apellido del cliente: ");
+    let cuenta = parseInt(prompt("Ingrese el tipo de cuenta que desea: "));
+    let clienteModificado = new Cliente (nombre, apellido, dni, cuenta)
+
+    arrayClientes.splice(indice, 1, clienteModificado);
+    console.log(arrayClientes);
+}
+
+// Funcion para consultar un cliente
+
+function consultaCliente() {
+    let dni = parseInt(prompt("Ingrese el DNI del clinte: "));
+    let cliente = arrayClientes.find(cliente => cliente.dni === dni);
+    console.log(cliente);
+}
+
+// Funcion para salir del programa
+
+function salir() {
+    alert("Usted salió del programa, nos vemos.")
+}
+
+// Ejecutando el programa
+
+let opcion = menu();
+
+switch(opcion){
+    case 1:
+        altaCliente();
+        break;
+    case 2:
+        bajaCliente();
+        break;
+    case 3:
+        modificacionCliente();
+        break;
+    case 4:
+        consultaCliente();
+        break
+    case 5:
+        salir();
+        break
+    default:
+        alert("Vuelva a seleccionar una opción")
+        break;
 }
